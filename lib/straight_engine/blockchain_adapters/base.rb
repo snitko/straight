09a6b1f416395cea827d450d4dd5ee91fcb601af
Roman Snitko <roman.snitko@gmail.com>
2014-08-16 04:58:57 +0400

    BlockchainAdapter::Base as an abstract class for all blockchain adapters

---
+++ lib/straight_engine/blockchain_adapters/base.rb
@@ -0,0 +1,28 @@
+module StraightEngine
+  module BlockchainAdapter
+
+    # An almost abstract class, providing guidance for the interfaces of
+    # all blockchain adapters as well as supplying some useful methods.
+    class Base
+
+      attr_accessor :api_base_url
+      
+      # Returns transaction info for the tid
+      def fetch_transaction(tid)
+      end
+
+      # Returns all transactions for the address
+      def fetch_transactions_for(address)
+      end
+
+      private
+
+        # Converts transaction info recieved from the source into the
+        # unified format expected by users of BlockchainAdapter instances.
+        def straighten_transaction(transaction)
+        end
+
+    end
+
+  end
+end

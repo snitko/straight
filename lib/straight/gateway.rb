8580bdb03078cc6cf64b226f105bc1aa5c5fccf9
Roman Snitko <roman.snitko@gmail.com>
2014-09-24 16:28:58 +0400

    Use two submodules for (Gateway|Order)Module, Prependable and Includable
    The reason for that decision is to not mess with the ancestor methods chain
    for those methods that shall not be augmented by the library the module is being included into.
    That is, if we include OrderModule into a class that supports saving attributes into
    the DB, it is only necessary to prepend getters and setters of the attributes, but
    not other methods that are of no concern to that class.
    
    The general rule for the Straight library being that for now we only prepend
    getters and setters while everything else is actually included.

--- lib/straight/gateway.rb
+++ lib/straight/gateway.rb
@@ -5,7 +5,7 @@ module Straight
     # Only add getters and setters for those properties in the extended class
     # that don't already have them. This is very useful with ActiveRecord for example
     # where we don't want to override AR getters and setters that set attributes.
-    def self.prepended(base)
+    def self.included(base)
       base.class_eval do
         [
           :pubkey,
@@ -18,6 +18,8 @@ module Straight
         ].each do |field|
           attr_reader field unless base.method_defined?(field)
           attr_writer field unless base.method_defined?("#{field}=")
+          prepend Prependable
+          include Includable
         end
       end
     end
@@ -33,75 +35,84 @@ module Straight
       return { period: period, iteration_index: iteration_index }
     end
 
-    # Creates a new order for the address derived from the pubkey and the keychain_id argument provided.
-    # See explanation of this keychain_id argument is in the description for the #address_for_keychain_id method.
-    def order_for_keychain_id(amount:, keychain_id:)
-      order             = Kernel.const_get(order_class).new
-      order.amount      = amount
-      order.gateway     = self
-      order.address     = address_for_keychain_id(keychain_id)
-      order.keychain_id = keychain_id
-      order
+    module Prependable
+      # Only getters and setters for attributes should be here
     end
 
-    # Returns a Base58-encoded Bitcoin address to which the payment transaction
-    # is expected to arrive. id is an an integer > 0 (hopefully not too large and hopefully
-    # the one a user of this class is going to properly increment) that is used to generate a
-    # an BIP32 bitcoin address deterministically.
-    def address_for_keychain_id(id)
-      keychain.node_for_path(id.to_s).to_address
-    end
-    
-    def fetch_transaction(tid, address: nil)
-      try_blockchain_adapters { |b| b.fetch_transaction(tid, address: address) }
-    end
-    
-    def fetch_transactions_for(address)
-      try_blockchain_adapters { |b| b.fetch_transactions_for(address) }
-    end
-    
-    def fetch_balance_for(address)
-      try_blockchain_adapters { |b| b.fetch_balance_for(address) }
-    end
+    module Includable
 
-    def keychain
-      @keychain ||= MoneyTree::Node.from_serialized_address(@pubkey)
-    end
-
-    # This is a callback method called from each order
-    # whenever an order status changes.
-    def order_status_changed(order)
-      @order_callbacks.each do |c|
-        c.call(order)
+      # Creates a new order for the address derived from the pubkey and the keychain_id argument provided.
+      # See explanation of this keychain_id argument is in the description for the #address_for_keychain_id method.
+      def order_for_keychain_id(amount:, keychain_id:)
+        order             = Kernel.const_get(order_class).new
+        order.amount      = amount
+        order.gateway     = self
+        order.address     = address_for_keychain_id(keychain_id)
+        order.keychain_id = keychain_id
+        order
       end
-    end
 
-    private
+      # Returns a Base58-encoded Bitcoin address to which the payment transaction
+      # is expected to arrive. id is an an integer > 0 (hopefully not too large and hopefully
+      # the one a user of this class is going to properly increment) that is used to generate a
+      # an BIP32 bitcoin address deterministically.
+      def address_for_keychain_id(id)
+        keychain.node_for_path(id.to_s).to_address
+      end
       
-      # Calls the block with each blockchain adapter until one of them does not fail.
-      # Fails with the last exception.
-      def try_blockchain_adapters(&block)
-        last_exception = nil
-        @blockchain_adapters.each do |adapter|
-          begin
-            result = yield(adapter)
-            last_exception = nil
-            return result
-          rescue Exception => e
-            last_exception = e
-            # If an Exception is raised, it passes on
-            # to the next adapter and attempts to call a method on it.
-          end
-        end
-        raise last_exception if last_exception
+      def fetch_transaction(tid, address: nil)
+        try_blockchain_adapters { |b| b.fetch_transaction(tid, address: address) }
       end
+      
+      def fetch_transactions_for(address)
+        try_blockchain_adapters { |b| b.fetch_transactions_for(address) }
+      end
+      
+      def fetch_balance_for(address)
+        try_blockchain_adapters { |b| b.fetch_balance_for(address) }
+      end
+
+      def keychain
+        @keychain ||= MoneyTree::Node.from_serialized_address(@pubkey)
+      end
+
+      # This is a callback method called from each order
+      # whenever an order status changes.
+      def order_status_changed(order)
+        @order_callbacks.each do |c|
+          c.call(order)
+        end
+      end
+
+      private
+        
+        # Calls the block with each blockchain adapter until one of them does not fail.
+        # Fails with the last exception.
+        def try_blockchain_adapters(&block)
+          last_exception = nil
+          @blockchain_adapters.each do |adapter|
+            begin
+              result = yield(adapter)
+              last_exception = nil
+              return result
+            rescue Exception => e
+              last_exception = e
+              # If an Exception is raised, it passes on
+              # to the next adapter and attempts to call a method on it.
+            end
+          end
+          raise last_exception if last_exception
+        end
+
+    end
+
 
 
   end
 
   class Gateway
 
-    prepend GatewayModule
+    include GatewayModule
 
     def initialize
       @blockchain_adapters   = [BlockchainInfoAdapter.mainnet_adapter, HelloblockIoAdapter.mainnet_adapter]
--- lib/straight/order.rb
+++ lib/straight/order.rb
@@ -12,12 +12,14 @@ module Straight
     # Only add getters and setters for those properties in the extended class
     # that don't already have them. This is very useful with ActiveRecord for example
     # where we don't want to override AR getters and setters that set attribtues.
-    def self.prepended(base)
+    def self.included(base)
       base.class_eval do
         [:amount, :address, :gateway, :keychain_id, :status, :tid].each do |field|
           attr_reader field unless base.method_defined?(field)
           attr_writer field unless base.method_defined?("#{field}=")
         end
+        prepend Prependable
+        include Includable
       end
     end
 
@@ -35,125 +37,131 @@ module Straight
     }
 
     class IncorrectAmount < Exception; end
-    
-    # Returns an array of transactions for the order's address, each as a hash:
-    #   [ {tid: "feba9e7bfea...", amount: 1202000, ...} ]
-    #
-    # An order is supposed to have only one transaction to its address, but we cannot
-    # always guarantee that (especially when a merchant decides to reuse the address
-    # for some reason -- he shouldn't but you know people).
-    #
-    # Therefore, this method returns all of the transactions.
-    # For compliance, there's also a #transaction method which always returns
-    # the last transaction made to the address.
-    def transactions(reload: false)
-      super if defined?(super)
-      @transactions = gateway.fetch_transactions_for(address) if reload || !@transactions
-      @transactions
-    end
 
-    # Last transaction made to the address. Always use this method to check whether a transaction
-    # for this order has arrived. We pick last and not first because an address may be reused and we
-    # always assume it's the last transaction that we want to check.
-    def transaction(reload: false)
-      super if defined?(super)
-      transactions(reload: reload).first
-    end
+    module Prependable
 
-    # Checks #transaction and returns one of the STATUSES based
-    # on the meaning of each status and the contents of transaction
-    # If as_sym is set to true, then each status is returned as Symbol, otherwise
-    # an equivalent Integer from STATUSES is returned.
-    def status(as_sym: false, reload: false)
-      @status = super() if defined?(super)
-      # Prohibit status update if the order was paid in some way.
-      # This is just a caching workaround so we don't query
-      # the blockchain needlessly. The actual safety switch is in the setter.
-      # Therefore, even if you remove the following line, status won't actually
-      # be allowed to change.
-      return @status if @status && @status > 1 
+      # Checks #transaction and returns one of the STATUSES based
+      # on the meaning of each status and the contents of transaction
+      # If as_sym is set to true, then each status is returned as Symbol, otherwise
+      # an equivalent Integer from STATUSES is returned.
+      def status(as_sym: false, reload: false)
+        @status = super() if defined?(super)
+        # Prohibit status update if the order was paid in some way.
+        # This is just a caching workaround so we don't query
+        # the blockchain needlessly. The actual safety switch is in the setter.
+        # Therefore, even if you remove the following line, status won't actually
+        # be allowed to change.
+        return @status if @status && @status > 1 
 
-      if reload || !@status
-        t = transaction(reload: reload)
-        self.status = if t.nil?
-          STATUSES[:new]
-        else
-          if t[:confirmations] >= gateway.confirmations_required
-            if t[:total_amount] == amount
-              STATUSES[:paid]
-            elsif t[:total_amount] < amount
-              STATUSES[:underpaid]
-            else
-              STATUSES[:overpaid]
-            end
+        if reload || !@status
+          t = transaction(reload: reload)
+          self.status = if t.nil?
+            STATUSES[:new]
           else
-            STATUSES[:unconfirmed]
+            if t[:confirmations] >= gateway.confirmations_required
+              if t[:total_amount] == amount
+                STATUSES[:paid]
+              elsif t[:total_amount] < amount
+                STATUSES[:underpaid]
+              else
+                STATUSES[:overpaid]
+              end
+            else
+              STATUSES[:unconfirmed]
+            end
           end
         end
+        as_sym ? STATUSES.invert[@status] : @status 
       end
-      as_sym ? STATUSES.invert[@status] : @status 
+
+      def status=(new_status)
+        # Prohibit status update if the order was paid in some way,
+        # so statuses above 1 are in fact immutable.
+        return false if @status && @status > 1
+
+        self.tid = transaction[:tid] if transaction
+        
+        # Pay special attention to the order of these statements. If you place
+        # the assignment @status = new_status below the callback call,
+        # you may get a "Stack level too deep" error if the callback checks
+        # for the status and it's nil (therefore, force reload and the cycle continues).
+        # 
+        # The order in which these statements currently are prevents that error, because
+        # by the time a callback checks the status it's already set.
+        @status_changed = (@status != new_status)
+        @status         = new_status
+        gateway.order_status_changed(self) if @status_changed
+        super if defined?(super)
+      end
+
     end
 
-    def status=(new_status)
-      # Prohibit status update if the order was paid in some way,
-      # so statuses above 1 are in fact immutable.
-      return false if @status && @status > 1
+    module Includable
 
-      self.tid = transaction[:tid] if transaction
+      # Returns an array of transactions for the order's address, each as a hash:
+      #   [ {tid: "feba9e7bfea...", amount: 1202000, ...} ]
+      #
+      # An order is supposed to have only one transaction to its address, but we cannot
+      # always guarantee that (especially when a merchant decides to reuse the address
+      # for some reason -- he shouldn't but you know people).
+      #
+      # Therefore, this method returns all of the transactions.
+      # For compliance, there's also a #transaction method which always returns
+      # the last transaction made to the address.
+      def transactions(reload: false)
+        super if defined?(super)
+        @transactions = gateway.fetch_transactions_for(address) if reload || !@transactions
+        @transactions
+      end
+
+      # Last transaction made to the address. Always use this method to check whether a transaction
+      # for this order has arrived. We pick last and not first because an address may be reused and we
+      # always assume it's the last transaction that we want to check.
+      def transaction(reload: false)
+        super if defined?(super)
+        transactions(reload: reload).first
+      end
+
+      # Starts a loop which calls #status(reload: true) according to the schedule
+      # determined in @status_check_schedule. This method is supposed to be
+      # called in a separate thread, for example:
+      #
+      #   Thread.new do
+      #     order.start_periodic_status_check
+      #   end
+      #
+      def start_periodic_status_check
+        check_status_on_schedule
+      end
       
-      # Pay special attention to the order of these statements. If you place
-      # the assignment @status = new_status below the callback call,
-      # you may get a "Stack level too deep" error if the callback checks
-      # for the status and it's nil (therefore, force reload and the cycle continues).
-      # 
-      # The order in which these statements currently are prevents that error, because
-      # by the time a callback checks the status it's already set.
-      @status_changed = (@status != new_status)
-      @status         = new_status
-      gateway.order_status_changed(self) if @status_changed
-      super if defined?(super)
-    end
-
-    # Starts a loop which calls #status(reload: true) according to the schedule
-    # determined in @status_check_schedule. This method is supposed to be
-    # called in a separate thread, for example:
-    #
-    #   Thread.new do
-    #     order.start_periodic_status_check
-    #   end
-    #
-    def start_periodic_status_check
-      super if defined?(super)
-      check_status_on_schedule
-    end
-    
-    def check_status_on_schedule(period: 10, iteration_index: 0)
-      super if defined?(super)
-      self.status(reload: true)
-      schedule = gateway.status_check_schedule.call(period, iteration_index)
-      if schedule && self.status < 2 # Stop checking if status is >= 2
-        sleep period
-        check_status_on_schedule(
-          period:          schedule[:period],
-          iteration_index: schedule[:iteration_index]
-        )
-      else
-        self.status = STATUSES[:expired]
+      def check_status_on_schedule(period: 10, iteration_index: 0)
+        self.status(reload: true)
+        schedule = gateway.status_check_schedule.call(period, iteration_index)
+        if schedule && self.status < 2 # Stop checking if status is >= 2
+          sleep period
+          check_status_on_schedule(
+            period:          schedule[:period],
+            iteration_index: schedule[:iteration_index]
+          )
+        else
+          self.status = STATUSES[:expired]
+        end
       end
-    end
 
-    def to_json
-      to_h.to_json
-    end
+      def to_json
+        to_h.to_json
+      end
 
-    def to_h
-      { status: status, amount: amount, address: address, tid: tid }
+      def to_h
+        { status: status, amount: amount, address: address, tid: tid }
+      end
+
     end
 
   end
 
   class Order
-    prepend OrderModule
+    include OrderModule
 
     def initialize
       @status = 0

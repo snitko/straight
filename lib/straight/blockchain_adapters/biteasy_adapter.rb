6ff63795473fbfb750d391c3e3430fe223a99731
aleks tkach <ale.tkachenko@gmail.com>
2015-01-21 07:15:04 -0600

    added more tests to Blockchain::BiteasyAdapter spec

--- lib/straight/blockchain_adapters/biteasy_adapter.rb
+++ lib/straight/blockchain_adapters/biteasy_adapter.rb
@@ -17,8 +17,6 @@ module Straight
 
       # Returns the current balance of the address
       def fetch_balance_for(address)
-        # I would like to check if address in returning hash matches our address in passed params
-        # seems like it will be safer
         JSON.parse(http_request("#{@base_url}/addresses/#{address}"))['data']['balance']
       end
 
--- spec/lib/blockchain_adapters/biteasy_adapter_spec.rb
+++ spec/lib/blockchain_adapters/biteasy_adapter_spec.rb
@@ -1,5 +1,4 @@
 require 'spec_helper'
-# require 'byebug'; debugger
 
 RSpec.describe Straight::Blockchain::BiteasyAdapter do
 

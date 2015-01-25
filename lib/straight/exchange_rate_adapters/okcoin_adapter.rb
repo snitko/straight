2a3c820de849c420689e2aca5e3a44e191fef372
aleks tkach <ale.tkachenko@gmail.com>
2015-01-24 21:16:54 -0600

    created okcoin_adapter file and initial code

---
+++ lib/straight/exchange_rate_adapters/okcoin_adapter.rb
@@ -0,0 +1,17 @@
+module Straight
+  module ExchangeRate
+
+    class OkcoinAdapter < Adapter
+      
+      FETCH_URL = 'https://www.okcoin.com/api/ticker.do?ok=1'
+
+      def rate_for(currency_code)
+        super
+        raise CurrencyNotSupported if currency_code != 'USD'
+        @rates['last'].to_f
+      end
+
+    end
+
+  end
+end

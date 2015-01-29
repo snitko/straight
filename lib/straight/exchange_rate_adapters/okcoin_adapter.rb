a434737fa16ae83559738d7f6a231b9d118bdef2
aleks tkach <ale.tkachenko@gmail.com>
2015-01-29 09:55:35 -0600

    Added #get_rate_value_from_hash to ExchangeRate::Adapter, altered #rate_for in OkcoinAdapter, added tests to OkcoinAdapter

--- lib/straight/exchange_rate_adapters/okcoin_adapter.rb
+++ lib/straight/exchange_rate_adapters/okcoin_adapter.rb
@@ -8,7 +8,8 @@ module Straight
       def rate_for(currency_code)
         super
         raise CurrencyNotSupported if currency_code != 'USD'
-        rate_to_f(@rates['ticker']['last'])
+        rate = get_rate_value_from_hash(@rates, 'ticker', 'last')
+        rate_to_f(rate)
       end
 
     end

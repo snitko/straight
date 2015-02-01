fe1b63d290bb27bb34a2b924eb2066997b928974
Roman Snitko <roman.snitko@gmail.com>
2015-02-02 01:52:27 +0700

    Fix: due to incorrect merge, we lost two "required" statements

--- lib/straight.rb
+++ lib/straight.rb
@@ -16,6 +16,8 @@ require_relative 'straight/exchange_rate_adapters/coinbase_adapter'
 require_relative 'straight/exchange_rate_adapters/bitstamp_adapter'
 require_relative 'straight/exchange_rate_adapters/localbitcoins_adapter'
 require_relative 'straight/exchange_rate_adapters/okcoin_adapter'
+require_relative 'straight/exchange_rate_adapters/btce_adapter'
+require_relative 'straight/exchange_rate_adapters/kraken_adapter'
 
 require_relative 'straight/order'
 require_relative 'straight/gateway'

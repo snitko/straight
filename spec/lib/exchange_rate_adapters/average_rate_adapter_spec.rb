032d1e62c6d2c958d8e18339279044c4be5c2d75
aleks tkach <ale.tkachenko@gmail.com>
2015-02-02 22:11:39 -0600

    added specs

--- spec/lib/exchange_rate_adapters/average_rate_adapter_spec.rb
+++ spec/lib/exchange_rate_adapters/average_rate_adapter_spec.rb
@@ -4,14 +4,17 @@ RSpec.describe Straight::ExchangeRate::AverageRateAdapter do
 
   before(:all) do
     @average_rates_adapter =  Straight::ExchangeRate::AverageRateAdapter.new(
-                                Straight::ExchangeRate::BitstampAdapter.new, 
-                                Straight::ExchangeRate::BitpayAdapter.new)
+                                Straight::ExchangeRate::BitstampAdapter, 
+                                Straight::ExchangeRate::BitpayAdapter.new,
+                              )
   end
 
   it "calculates average rate" do
-    expect(@average_rates_adapter.calculate_avg_rate).to be_kind_of Numeric
-    @average_rates_adapter.instance_variable_set('@rates', [8, nil, 2])
-    expect(@average_rates_adapter.calculate_avg_rate).to eq 5
+    expect(@average_rates_adapter.rate_for('USD')).to be_kind_of Numeric
+  end
+
+  it "raises exception if all adapters fail to get rates" do
+    expect( -> { @average_rates_adapter.rate_for('FEDcoin') }).to raise_error(Straight::ExchangeRate::Adapter::CurrencyNotSupported)
   end
 
 end

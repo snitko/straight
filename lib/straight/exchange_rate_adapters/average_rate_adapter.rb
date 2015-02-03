24819ad89350df63ee8f6666fa55c79f8dfa48dc
aleks tkach <ale.tkachenko@gmail.com>
2015-02-02 22:11:26 -0600

    Altered AverageRateAdapter to accept class names as an arguments, implemented raising CurrencyNotSupported if all adapters fail to get rates

--- lib/straight/exchange_rate_adapters/average_rate_adapter.rb
+++ lib/straight/exchange_rate_adapters/average_rate_adapter.rb
@@ -1,19 +1,29 @@
 module Straight
   module ExchangeRate
 
-    class AverageRateAdapter
+    class AverageRateAdapter < Adapter
 
-      # Takes exchange rate adapters instances as an arguments
-      def initialize(*adapter_instances)
-        @rates = []
-        adapter_instances.each do |adapter|
-          @rates << adapter.rate_for('USD')
-        end
+      # Takes exchange rate adapters instances or classes as an arguments
+      def initialize(*adapters)
+        @adapters = adapters.map{ |adapter| adapter.respond_to?(:new) ? adapter.new : adapter }
       end
 
-      def calculate_avg_rate
-        @rates.compact!
-        @rates.inject {|sum, rate| sum + rate} / @rates.size
+      def rate_for(currency_code)
+        rates = []
+        @adapters.each do |adapter| 
+          begin
+            rates << adapter.rate_for(currency_code)
+          rescue CurrencyNotSupported
+            rates << nil
+          end
+        end
+
+        unless rates.select(&:nil?).size == @adapters.size 
+          rates.compact!
+          rates.inject {|sum, rate| sum + rate} / rates.size
+        else
+          raise CurrencyNotSupported
+        end
       end
 
     end

f51a83f81eeacf988bd6cc6191226b334302404e
aleks tkach <ale.tkachenko@gmail.com>
2015-02-08 22:39:38 -0600

    Altered #rate_for in CoinbaseAdapter + specs

--- spec/lib/exchange_rate_adapters/coinbase_adapter_spec.rb
+++ spec/lib/exchange_rate_adapters/coinbase_adapter_spec.rb
@@ -11,4 +11,16 @@ RSpec.describe Straight::ExchangeRate::CoinbaseAdapter do
     expect( -> { @exchange_adapter.rate_for('FEDcoin') }).to raise_error(Straight::ExchangeRate::Adapter::CurrencyNotSupported)
   end
 
+  it "raises exception if rate is nil" do
+    json_response_1 = '{}'
+    json_response_2 = '{"btc_to_urd":"224.41","usd_to_xpf":"105.461721","bsd_to_btc":"0.004456"'
+    json_response_3 = '{"btc_to_usd":"null","usd_to_xpf":"105.461721","bsd_to_btc":"0.004456"}'
+    uri_mock = double('uri mock')
+    allow(uri_mock).to receive(:read).with(read_timeout: 4).and_return(json_response_1, json_response_2, json_response_3)
+    allow(URI).to      receive(:parse).and_return(uri_mock)
+    3.times do
+      expect( -> { @exchange_adapter.rate_for('USD') }).to raise_error(Straight::ExchangeRate::Adapter::CurrencyNotSupported)
+    end
+  end
+
 end

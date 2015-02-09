5a0787481cbee6d5b10160e0ab02017859b055b3
aleks tkach <ale.tkachenko@gmail.com>
2015-02-09 17:03:37 -0600

    Fix added #fetch_rates calls in specs for Okcoin, Localbitcoins, Kraken, Btce adapters + fixed response_hashes in specs

--- spec/lib/exchange_rate_adapters/localbitcoins_adapter_spec.rb
+++ spec/lib/exchange_rate_adapters/localbitcoins_adapter_spec.rb
@@ -14,11 +14,12 @@ RSpec.describe Straight::ExchangeRate::LocalbitcoinsAdapter do
   it "rases exception if rate is nil" do
     json_response_1 = '{"USD": {}}'
     json_response_2 = '{"USD": {"volume_btc": "2277.85", "rates": {"bambo": "263.78"}, "avg_1h": 287.6003904801631, "avg_24h": 253.58144543993674, "avg_12h": 252.29202866050034}}'
-    json_response_3 = '{"USD": {"volume_btc": "2277.85", "rates": {"last": "null"}, "avg_1h": 287.6003904801631, "avg_24h": 253.58144543993674, "avg_12h": 252.29202866050034}}'
+    json_response_3 = '{"USD": {"volume_btc": "2277.85", "rates": {"last": null}, "avg_1h": 287.6003904801631, "avg_24h": 253.58144543993674, "avg_12h": 252.29202866050034}}'
     uri_mock = double('uri mock')
     allow(uri_mock).to receive(:read).with(read_timeout: 4).and_return(json_response_1, json_response_2, json_response_3)
     allow(URI).to      receive(:parse).and_return(uri_mock)
     3.times do
+      @exchange_adapter.fetch_rates!
       expect( -> { @exchange_adapter.rate_for('USD') }).to raise_error(Straight::ExchangeRate::Adapter::CurrencyNotSupported)
     end
   end

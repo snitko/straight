313d645c56783ba71242f0f537c394b8ecebff8d
Roman Snitko <roman.snitko@gmail.com>
2015-05-05 03:25:20 +0700

    Mycelium WAPI blockchain adapter

---
+++ lib/straight/blockchain_adapters/mycelium_adapter.rb
@@ -0,0 +1,126 @@
+module Straight
+  module Blockchain
+
+    class MyceliumAdapter < Adapter
+
+      require 'base64'
+
+      def self.mainnet_adapter
+        self.new("https://mws2.mycelium.com/wapi/wapi")
+      end
+      
+      def self.testnet_adapter
+        raise "https://node3.mycelium.com/wapitestnet/wapi"
+      end
+      
+      def initialize(base_url)
+        @latest_block = { cache_timestamp: nil, block: nil }
+        @base_url = base_url
+      end
+
+      # Returns transaction info for the tid
+      def fetch_transaction(tid, address: nil)
+        transaction = api_request('getTransactions', { txIds: [tid] })['transactions'].first
+        straighten_transaction transaction, address: address
+      end
+
+      # Supposed to returns all transactions for the address, but
+      # currently actually returns the first one, since we only need one.
+      def fetch_transactions_for(address)
+        tid = api_request('queryTransactionInventory', { addresses: [address], limit: 1 })["txIds"].first
+        tid ? [fetch_transaction(tid, address: address)] : []
+      end
+
+      # Returns the current balance of the address
+      def fetch_balance_for(address)
+        unspent = 0
+        api_request('queryUnspentOutputs', { addresses: [address]})['unspent'].each do |out|
+          unspent += out['value']
+        end
+        unspent
+      end
+
+      # Here we are using Blockchain.info API at this point because I'm not sure how
+      # to get latest block from Mycelium WAPI
+      def latest_block(force_reload: false)
+        # If we checked Blockchain.info latest block data
+        # more than a minute ago, check again. Otherwise, use cached version.
+        if @latest_block[:cache_timestamp].nil?              ||
+           @latest_block[:cache_timestamp] < (Time.now - 60) ||
+           force_reload
+          @latest_block = {
+            cache_timestamp: Time.now,
+            block: JSON.parse(HTTParty.get("https://blockchain.info/latestblock", timeout: 4, verify: false).body)
+          }
+        else
+          @latest_block
+        end
+      end
+
+      private
+
+        def api_request(method, params={})
+          begin
+            body = JSON.parse(HTTParty.post(
+              "#{@base_url}/#{method}",
+              body: params.merge({version: 1}).to_json,
+              headers: { 'Content-Type' => 'application/json' },
+              timeout: 15,
+              verify: false
+            ).body)["r"]
+          rescue HTTParty::Error => e
+            raise RequestError, YAML::dump(e)
+          rescue JSON::ParserError => e
+            raise RequestError, YAML::dump(e)
+          end
+        end
+
+        # Converts transaction info received from the source into the
+        # unified format expected by users of BlockchainAdapter instances.
+        def straighten_transaction(transaction, address: nil)
+
+          # Get the block number this transaction was included into
+          block_height = transaction['height']
+          tid          = transaction['txid']
+
+          # Converting from Base64 to hex
+          transaction = transaction['binary'].unpack("m0").first.unpack("H*").first
+
+          # Decoding with bitcoin-cli
+          transaction = JSON.parse(`bitcoin-cli decoderawtransaction #{transaction}`)
+
+          outs         = []
+          total_amount = 0
+          transaction['vout'].each do |out|
+            out['value'] = out['value']*10**8
+            total_amount += out['value'] if address.nil? || address == out['address']
+            outs << { amount: out['value'], receiving_address: out['address'] }
+          end
+
+          {
+            tid:           tid,
+            total_amount:  total_amount,
+            confirmations: calculate_confirmations(block_height),
+            outs:          outs
+          }
+        end
+
+
+        # When we call #calculate_confirmations, it doesn't always make a new
+        # request to the blockchain API. Instead, it checks if cached_id matches the one in
+        # the hash. It's useful when we want to calculate confirmations for all transactions for
+        # a certain address without making any new requests to the Blockchain API.
+        def calculate_confirmations(block_height, force_latest_block_reload: false)
+
+          if block_height && block_height != -1
+            latest_block(force_reload: force_latest_block_reload)[:block]["height"] - block_height + 1
+          else
+            0
+          end
+
+        end
+
+    end
+
+  end
+end
---
+++ spec/lib/blockchain_adapters/mycelium_adapter_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+RSpec.describe Straight::Blockchain::MyceliumAdapter do
+
+  subject(:adapter) { Straight::Blockchain::MyceliumAdapter.mainnet_adapter }
+
+  it "fetches all transactions for the current address" do
+    address = "3B1QZ8FpAaHBgkSB5gFt76ag5AW9VeP8xp"
+    expect(adapter).to receive(:straighten_transaction).with(anything, address: address).at_least(:once)
+    expect(adapter.fetch_transactions_for(address)).not_to be_empty
+  end
+
+  it "fetches the balance for a given address" do
+    address = "1NX8bgWdPq2NahtTbTUAAdsTwpMpvt7nLy"
+    expect(adapter.fetch_balance_for(address)).to be_kind_of(Integer)
+  end
+
+  it "fetches a single transaction" do
+    tid = 'ae0d040f48d75fdc46d9035236a1782164857d6f0cca1f864640281115898560'
+    expect(adapter.fetch_transaction(tid)[:total_amount]).to eq(832947)
+  end
+
+  it "calculates the number of confirmations for each transaction" do
+    tid = 'ae0d040f48d75fdc46d9035236a1782164857d6f0cca1f864640281115898560'
+    expect(adapter.fetch_transaction(tid)[:confirmations]).to be > 0
+  end
+
+  it "gets a transaction id among other data" do
+    tid = 'ae0d040f48d75fdc46d9035236a1782164857d6f0cca1f864640281115898560'
+    expect(adapter.fetch_transaction(tid)[:tid]).to eq(tid)
+  end
+
+  it "caches blockchain.info latestblock requests" do
+    latest_block_response = double('Blockchain info latest block response')
+    expect(latest_block_response).to receive(:body).and_return('{ "height": 1 }') 
+    expect(HTTParty).to receive(:get).with("https://blockchain.info/latestblock", timeout: 4, verify: false).once.and_return(latest_block_response)
+    adapter.send(:calculate_confirmations, 1, force_latest_block_reload: true)
+    adapter.send(:calculate_confirmations, 1)
+    adapter.send(:calculate_confirmations, 1)
+    adapter.send(:calculate_confirmations, 1)
+    adapter.send(:calculate_confirmations, 1)
+  end
+  
+  it "raises an exception when something goes wrong with fetching datd" do
+    expect( -> { adapter.send(:api_request, "/a-404-request") }).to raise_error(Straight::Blockchain::Adapter::RequestError)
+  end
+
+end

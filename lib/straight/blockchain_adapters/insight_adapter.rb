d175d9ef7b068c6d02d855469e5187acb996f9d5
Dmitry Tymchuk <dsnipe@gmail.com>
2015-07-10 15:34:08 +0300

    Fix: return correct total_amount for Insight

--- lib/straight/blockchain_adapters/insight_adapter.rb
+++ lib/straight/blockchain_adapters/insight_adapter.rb
@@ -3,28 +3,31 @@ module Straight
     
     class InsightAdapter < Adapter
 
-      def self.mainnet_adapter(host_url)
-        new(host_url)
+      @@test_url = nil
+
+      def self.mainnet_adapter(main_url:, test_url: nil)
+        @@test_url = test_url
+        new(main_url)
       end
 
       def self.testnet_adapter
-        raise "Not Supported Yet"
+        raise "Testnet not implemented" unless @@test_url
+        new(@@test_url)
       end
 
       def initialize(host_url)
         @base_url = host_url
       end
 
-      def fetch_transaction(tid)
+      def fetch_transaction(tid, address: nil)
         res = api_request("/tx/", tid)
-        straighten_transaction(res)
+        straighten_transaction(res, address: address)
       end
 
       def fetch_transactions_for(address)
         res = api_request("/addr/", address)
-        raise BitcoinAddressInvalid, message: "address in question: #{address}" unless res
         return [] if res["transactions"].empty?
-        [fetch_transaction(res["transactions"].first)]
+        [fetch_transaction(res["transactions"].first, address: address)]
       end
 
       def fetch_balance_for(address)
@@ -42,22 +45,26 @@ module Straight
           timeout: 15,
           verify: false
         ).body
-        JSON.parse(res || "")
+        JSON.parse(res)
       rescue HTTParty::Error => e
         raise RequestError, YAML::dump(e)
       rescue JSON::ParserError => e
+        raise BitcoinAddressInvalid, message: "address in question: #{val}" if e.message.include?("Invalid address")
         raise RequestError, YAML::dump(e)
       end
 
-      def straighten_transaction(transaction)
+      def straighten_transaction(transaction, address: nil)
+        total_amount = 0
         tid = transaction["txid"]
-        total_amount = transaction["valueOut"]
+        transaction["vout"].each do |o|
+          total_amount += Satoshi.new(o["value"]) if address.nil? || address == o["scriptPubKey"]["addresses"].first
+        end
         confirmations = transaction["confirmations"] 
         outs = transaction["vout"].map { |o| {amount: Satoshi.new(o["value"]).to_i, receiving_address: o["scriptPubKey"]["addresses"].first} }
 
         {
           tid:           tid,
-          total_amount:  Satoshi.new(total_amount).to_i,
+          total_amount:  total_amount,
           confirmations: confirmations || 0,
           outs:          outs || []
         }
